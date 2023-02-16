a8709a6690948fd9415ff17a7b66fb67abea9c39
Kim <kimbvundura@gmail.com>
2023-02-16 05:34:48 -0800

    t5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - A program that print a line using the printf function
  * Return: 0
  */
-int main(void)
+
+int main (void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
 }	
