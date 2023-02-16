eae9d06c27f9d685c652301d3e317f26a8d6b6ce
Kim <kimbvundura@gmail.com>
2023-02-16 09:18:29 -0800

    tas6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of char: %i byte(s)\n", sizeof(char));
-	printf("Size of int: %i byte(s)\n", sizeof(int));
-	printf("Size of long int: %i byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %i byte(s)\n", sizeof(long long long int));
-	printf("Size of float: %i byte(s)\n", sizeof(float));
+	printf("Size a of char: %d byte(s)\n", sizeof(char));
+	printf("Size an of int: %d byte(s)\n", sizeof(int));
+	printf("Size a of long int: %d byte(s)\n", sizeof(long int));
+	printf("Size a of long long int: %d byte(s)\n", sizeof(long long long int));
+	printf("Size a of float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
