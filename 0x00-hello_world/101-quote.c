5005172b73107cc09d1362dbed825c5663d80886
Kim <kimbvundura@gmail.com>
2023-02-16 09:42:50 -0800

    t.8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<unistd.h>
+
 /**
  * main - Entry
  * Return: 1
  */
+
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
