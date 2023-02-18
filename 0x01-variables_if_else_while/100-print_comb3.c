a89b72c1135de5ee321240c6db03ee333ad8718d
Kim <kimbvundura@gmail.com>
2023-02-17 16:07:46 -0800

    task11

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Entry point
- *
+ * main - Entry poiny
  * Return: Always 0
  */
- int main(void)
+int main(void)
 {
 	int ones = '0';
 	int tens = '0';
@@ -17,8 +16,7 @@
 			{
 				putchar(tens);
 				putchar(ones);
-				if (!(ones == '9' && tens == '8'))/*addes comma and space*/
-
+				if (!(ones == '9' && tens == '0'))/*addes comma and space*/
 					{
 						putchar(',');
 						putchar(' ');
