f456493b2f9407853b69a761cf2c6d99432b4f4d
iluvkim <kimbvundura@gmail.com>
2023-04-03 05:57:09 -0700

    0x13

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - Frees a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ *
+ * Description: Sets the head to NULL.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	do {
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	} while (*head != NULL);
+
+	*head = NULL;
+}
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - Delete the first element of a singly linked list.
+ * @head: Pointer to a list.
+ * Return: head node's data (n) or 0 if the link list is empty
+ *
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int n;
+
+	if (*head == NULL)
+		return (0);
+
+	n = (*head)->n;
+	temp = *head;
+
+	do {
+		*head = (*head)->next;
+		free(tmp);
+	} while (0);
+
+	return (n);
+}
