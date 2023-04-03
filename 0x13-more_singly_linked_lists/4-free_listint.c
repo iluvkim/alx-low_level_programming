0ff4962aa363baaa7d29c37bb20c549042029f05
iluvkim <kimbvundura@gmail.com>
2023-04-03 05:26:43 -0700

    0x13

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,10 +9,9 @@ void free_listint(listint_t *head)
 {
 	listint_t *tmp;
 
-	while (head != NULL)
-	{
-		tmp = head->next;
-		free(head);
-		head = tmp;
-	}
+	do {
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	} while (head != NULL);
 }
