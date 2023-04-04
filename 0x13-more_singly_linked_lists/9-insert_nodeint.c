978625ae6a68ce58c51d024de4c30e5d2a8740b1
iluvkim <kimbvundura@gmail.com>
2023-04-04 05:21:02 -0700

    0x13

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,9 +37,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		tmp->next = new;
 	} else
 	{
-		free(new_node);
+		free(new);
 		return (NULL);
 	}
 
-return (new_node);
+return (new);
 	}
